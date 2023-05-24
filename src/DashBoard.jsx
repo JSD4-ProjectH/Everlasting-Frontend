98efda54523983c183a2651a93cab60e5d8ffe16
Wichy007 <naravitdansiriviroj@gmail.com>
2023-05-24 20:52:38 +0700

    fix

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -11,7 +11,7 @@ import AddBtn from './component/AddBtn';
 import Layout from "./Navbar/Layout";
 import DashboardLabel from "./component/DasboardLabel";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Dashboard.css'
+import './DashBoard.css'
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { useNavigate } from 'react-router-dom'
